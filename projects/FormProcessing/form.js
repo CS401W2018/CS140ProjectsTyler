aef6ef00548867ef40deee3baadd756cf896de62
Fiery51 <tylerjackmo@outlook.com>
2024-11-16 12:25:54 -1000

    Update form.js

--- projects/FormProcessing/form.js
+++ projects/FormProcessing/form.js
@@ -4,7 +4,9 @@ document.getElementById("myForm").addEventListener("submit", function (event) {
     //Grab all teh document valuessssss
     const first = document.getElementById("fname").value;
     const last = document.getElementById("lname").value;
+    const pass = document.getElementById("password").value
     const age = document.getElementById("age").value;
+    const location = document.getElementById("location").value
     const TOS = document.getElementById("TOS").checked;
 
     //input validation wooooo
@@ -13,8 +15,19 @@ document.getElementById("myForm").addEventListener("submit", function (event) {
         return;
     }
 
-    if (!(age >= 18)) {
+    //Checks if age is null, if the input is not a number, and if the user is 18 or older, if any of these are true than we go into the if statement
+    if (!age || isNaN(age) || age <= 18) {
         alert("You must be 18 or older to fill out this form.");
+        return;
+    }
+
+    if(!pass){
+        alert("Please create a password")
+        return;
+    }
+
+    if(!location){
+        alert("Please pick a location!")
         return;
     }
 
@@ -27,8 +40,8 @@ document.getElementById("myForm").addEventListener("submit", function (event) {
     const formData = {
         firstName: first,
         lastName: last,
-        password: document.getElementById('password').value,
-        state: document.getElementById("location").value
+        password: pass,
+        location: location
     };
 
     //Log the form data object!
@@ -40,10 +53,10 @@ document.getElementById("myForm").addEventListener("submit", function (event) {
     xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
     xhr.onreadystatechange = function () {
         if (xhr.readyState === 4 && xhr.status === 200) {
-            alert("Form processed successfully.");
             const response = JSON.parse(xhr.responseText);
             document.getElementById("message").innerHTML = response.message;
             document.getElementById("myForm").innerHTML = "";
+            alert("Form processed successfully.");
         } else if (xhr.readyState === 4) {
             alert("Error processing form.");
         }
